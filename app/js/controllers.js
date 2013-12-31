92e63d6d415a6ccaf246dda0643a0fbf5a2c9aa2
Aaron Lieberman <aaron@bigcompass.com>
2013-12-31 14:31:04 -0700

    Test Get

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -26,8 +26,9 @@ angular.module('Services', ['ngResource', 'ngRoute'])
 	$scope.$routeParams = $routeParams;
 	$scope.params = $routeParams;
 	$scope.feedList = Feed.query();
+	$scope.feedList1 = angular.toJson($scope.feedList);
 	$scope.singleFeed = Feed.get({id:$routeParams.id});
-	console.log($scope.feedList);
+	console.log($scope.feedList1);
 	
 	
 	//$scope.params = $scope.feedList.params;
