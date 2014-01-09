96deddea8c45ff505f27e7a7fdec67e1b553d955
Aaron Lieberman <aaron@bigcompass.com>
2014-01-09 14:26:58 -0700

    Edit Ctrl PUT Attempt

--- app/js/controllers/FeedEditCtrl.js
+++ app/js/controllers/FeedEditCtrl.js
@@ -9,29 +9,23 @@ mftApp.controller('FeedEditCtrl', ['$scope', '$resource', 'SingleFeed', '$routeP
 	$scope.$routeParams = $routeParams;
 	$scope.params = $routeParams;
 	return $scope.singleFeed = SingleFeed.get({id: $routeParams.id});
-	/*	$scope.editFeed = function() {
-			$scope.feedList.push({interface:$scope.feed.interface});
-			alert($scope.feed.interface)
-			$scope.feedList = Feed.$save();
-		};*/
 
-		$scope.editFeed = function() {
-		  //$scope.Services.$save();
-		  //$location.path('index.html#/feeds/$routeParams.id/$routeParams.status/$routeParams.interface'); //{{feed.id}}/{{feed.status}}/{{feed.interface}}');
-		  var updateFeed = {};
-		  var nf = new SingleFeed(updateFeed);
-		  nf.$update(function() {
+	$scope.editFeed = function() {
+		SingleFeed.update($scope.singleFeed, function(data){
+			$scope.singleFeed.push(data);
+		});
+	};
+  }
+]);	  
+	/*var updateFeed = {};
+	  var nf = new SingleFeed(updateFeed);
+		nf.$update(function() {
 			$scope.singleFeed.push(nf);
 		  });
-		};
+		};*/
 	
 
-  }
-]);
-
-
-
-
+  
 	
 
  
