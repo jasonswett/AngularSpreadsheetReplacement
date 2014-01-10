492d4317c8f938256f575c946fc074b81035d3a8
Aaron Lieberman <aaron@bigcompass.com>
2014-01-10 15:27:28 -0700

    Comment Post

--- app/js/services/CommentIndexService.js
+++ app/js/services/CommentIndexService.js
@@ -4,7 +4,7 @@
 
 mftApp.factory('Comment', ['$resource', function($resource) {
     //var getComment = $resource('JSON/comments.json', {id: '@id'},
- 	var getComment = $resource('http://dev.bigcompass.com\:2222/rest/AaronL/Comments/:id', {id: '@FEED_ID'}, 
+ 	var getComment = $resource('http://dev.bigcompass.com\:2222/rest/AaronL/Comments/:id', {id: '@results.FEED_ID'}, 
 		{ post: {method: 'POST'}, 
 		get: {method: 'GET'}, 
 		update: {method:'PUT'}, 
