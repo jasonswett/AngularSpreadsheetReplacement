f724f4981333d38152b94ad48c3ccd8e5ef51565
Aaron Lieberman <aaron@bigcompass.com>
2014-01-13 08:35:11 -0700

    Feed Edit

--- app/js/services/SingleFeedService.js
+++ app/js/services/SingleFeedService.js
@@ -5,9 +5,9 @@
 mftApp.factory('SingleFeed', ['$resource', function($resource) { 
 	//var getFeed = $resource('JSON/feeds.json', {id:'@id', status:'@status', interface:'@interface'},
 	var getFeed = $resource('http://dev.bigcompass.com\:2222/rest/AaronL/SingleFeed/:id', {id:'@ID'}, 	
-	{ post: {method: 'POST'}, 
+	{ post: {method: 'POST', transformRequest : function(data, headersGetter)}, 
 	get: {method: 'GET'}, 
-	update: {method:'PUT'}, 
+	update: {method:'PUT', transformRequest : function(data, headersGetter)}, 
 	query: {method:'GET', isArray:false}});
 	return getFeed;
 }]);
