f712f33ef1497b8c4794f53d46f156867bde6999
Aaron Lieberman <aaron@bigcompass.com>
2014-01-14 15:54:10 -0700

    Auditing

--- app/js/controllers/AuditFeedCtrl.js
+++ app/js/controllers/AuditFeedCtrl.js
@@ -2,18 +2,19 @@
 
 /* Enable Audit Trail On Feeds Controller */
 
-mftApp.controller('AuditFeedCtrl', ['$scope', '$resource', 'AuditFeed', '$routeParams', '$route', '$location', '$filter', 
-  function($scope, $resource, AuditFeed, $routeParams, $route, $location, $filter) {
+mftApp.controller('AuditFeedCtrl', ['$scope', '$resource', 'AuditFeed', '$routeParams', '$route', '$location', '$filter', 'SingleFeed', 
+  function($scope, $resource, AuditFeed, $routeParams, $route, $location, $filter, SingleFeed) {
 	$scope.$route = $route;
 	$scope.$location = $location;
 	$scope.$routeParams = $routeParams;
 	$scope.params = $routeParams;     
 	$scope.auditFeed = AuditFeed.get({id: $routeParams.id});
+	$scope.singleFeed = SingleFeed.get({id: $routeParams.id});
 
 	//Add Old Data To DB; POST to DB
 	$scope.audit = function() {
 		AuditFeed.post({id:$routeParams.id}, 
-		$scope.auditFeed, 
+		$scope.singleFeed, 
 		function() {
 			console.log("saved!");
 		},
