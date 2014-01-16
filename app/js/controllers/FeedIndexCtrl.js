5215ea148ec83754a339f36cdbb30b9e6cbf935c
Aaron Lieberman <aaron@bigcompass.com>
2014-01-16 10:23:43 -0700

    Infinite Scrolling

--- app/js/controllers/FeedIndexCtrl.js
+++ app/js/controllers/FeedIndexCtrl.js
@@ -20,7 +20,8 @@ mftApp.controller('FeedIndexCtrl', ['$scope', '$resource', 'Feed', '$routeParams
 			parseInt( res.results[i].ID );
 		    listOfFeeds[i] = res.results[i];
 		  }
-		
+		console.log("listOfFeeds.ID:" + typeof(listofFeeds.ID));
+		console.log("res.results.ID:" + typeof(res.results.ID));
 		$scope.feedList = listOfFeeds;
 		
 		
