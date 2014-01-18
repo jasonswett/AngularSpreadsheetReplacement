010e510369c3b80510de0dbbc2a6ef3138da89fc
Aaron Lieberman <aaron@bigcompass.com>
2014-01-17 18:32:54 -0700

    Get Event List

--- app/js/controllers/EventCtrl.js
+++ app/js/controllers/EventCtrl.js
@@ -4,12 +4,15 @@
 
 mftApp.controller('EventCtrl', ['$scope', '$resource', 'Event', '$routeParams', '$route', '$location', '$filter', 
   function($scope, $resource, Event, $routeParams, $route, $location, $filter) {
-	$scope.eventList = Event.query(function(results){
+	$scope.eventList = [];
+	Event.query(function(results){
 		angular.forEach(results.results, function(value, key){
+			value.DATA = angular.fromJson(value.DATA);
+			$scope.eventList.push(value);
 			
-			console.log(angular.fromJson(value.DATA));
 		} 
 		);
+		console.log($scope.eventList);
 		console.log(results.results[0]);
 	}
 	);
