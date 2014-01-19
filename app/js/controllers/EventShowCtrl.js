1edbb59de972f6bdc378e1b8d425691d7773c573
Aaron Lieberman <aaron@bigcompass.com>
2014-01-18 22:06:24 -0700

    Get Logged Event Attribute

--- app/js/controllers/EventShowCtrl.js
+++ app/js/controllers/EventShowCtrl.js
@@ -16,6 +16,7 @@ mftApp.controller('EventShowCtrl', ['$scope', '$resource', 'SingleEvent', '$rout
 		//Compare Old and New Values
 		for (var i = 1; i <= $scope.singleEvent.length; i++) {
 			console.log("1st For Loop");
+			console.log($scope.singleEvent[0].DATA.length);
 			for (var j = 1; j <= $scope.singleEvent[i-1].DATA.length; j++) {
 				console.log("1st For Loop");
 				if ($scope.singleEvent[i-1].DATA[j-1] != $scope.singleEvent[i].DATA[j]) {
