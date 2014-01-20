4f426b1cd033c154774b9f6e9500e6d4fee70d25
Aaron Lieberman <aaron@bigcompass.com>
2014-01-20 11:35:08 -0700

    Enable Admin/manage Login

--- app/js/controllers/UserIndexCtrl.js
+++ app/js/controllers/UserIndexCtrl.js
@@ -5,8 +5,6 @@
   mftApp.controller('UserIndexCtrl', function($scope, $location, $rootScope) {
 	$rootScope.loggedIn = false;
 	$scope.loginAttempt = false;
-	$scope.userAttempt = false;
-	$scope.passAttempt = false;
 	$scope.login = function() {
 		if ( $scope.user.email == "administrator@disney.com" && $scope.user.password == "manage" ) { // test
 			$rootScope.userName = $scope.user.email;
@@ -16,8 +14,6 @@
 		else {
 			$location.path("/login");
 			$scope.loginAttempt = true;
-			$scope.userAttempt = true;
-			$scope.passwordAttempt = true;
 		    $scope.loginError = "Invalid Email/Password";
 		}
 	};
