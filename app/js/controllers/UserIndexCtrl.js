24231805a79a636fdee05e1b8f00b9e83b36a454
Aaron Lieberman <aaron@bigcompass.com>
2014-01-20 13:47:09 -0700

    Enable Admin/manage Login

--- app/js/controllers/UserIndexCtrl.js
+++ app/js/controllers/UserIndexCtrl.js
@@ -5,8 +5,10 @@
   mftApp.controller('UserIndexCtrl', function($scope, $location, $rootScope) {
 	$rootScope.loggedIn = false;
 	$scope.loginAttempt = false;
+	$rootScope.user.email = $scope.user.email;
+	$rootScope.user.password = $scope.user.password;
 	$scope.login = function() {
-		if ( $scope.user.email == "ad" && $scope.user.password == "man" ) { // test
+		if ( $rootScope.user.email == "ad" && $rootScope.user.password == "man" ) { // test
 			$rootScope.userName = $scope.user.email;
 			$rootScope.loggedIn = true;
 		    $location.path( "/" );
