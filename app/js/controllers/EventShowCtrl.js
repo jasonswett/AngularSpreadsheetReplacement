4c4565e5bea03a9ce89b875573904a2573eebe3c
Aaron Lieberman <aaron@bigcompass.com>
2014-01-21 11:01:21 -0700

    Sort through columns of eventData

--- app/js/controllers/EventShowCtrl.js
+++ app/js/controllers/EventShowCtrl.js
@@ -12,13 +12,14 @@ mftApp.controller('EventShowCtrl', ['$scope', '$resource', 'SingleEvent', '$rout
 		angular.forEach(results.results, function(value, key){
 			value.DATA = angular.fromJson(value.DATA);
 			$scope.singleEvent.push(value);
-			$scope.eventData.push(value.DATA);
+			//$scope.eventData.push(value.DATA);
 		});
 		console.log($scope.eventData);
 		//Compare Old and New Values
 		for (var i = 0; i < $scope.eventData.length; i++) {
 			console.log("1st For Loop");
 			$scope.eventData[i] = [];
+			$scope.eventData[i] = value.DATA[i];
 			/*for (var j = 0; j < 66; j++) {
 				if ($scope.eventData[i-1] != $scope.eventData[i]) {
 					$scope.oldEvent.push($scope.eventData[i-1]);
