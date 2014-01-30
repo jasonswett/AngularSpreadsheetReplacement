0f740c85a29c2d258b6f9e8d35a1552048eafca5
Aaron Lieberman <aaron@bigcompass.com>
2014-01-30 13:34:15 -0700

    Success/Failure Messages and Code Cleanup

--- app/js/services/UserService.js
+++ app/js/services/UserService.js
@@ -2,10 +2,12 @@
 
 /* Query Comments From DB Service */
 
-mftApp.factory('UserAuth', ['$resource', function($resource) {
- 	var getUserInfo = $resource('http://dev.bigcompass.com\:2222/rest/AaronL/UserAuth', {}, 
+mftApp.factory('UserAuth', ['$cookieStore', '$resource', function($cookieStore, $resource) {
+	var getUserInfo = $resource('http://dev.bigcompass.com\:2222/rest/AaronL/UserAuth', {}, 
 		{ post: {method: 'POST', 
 		transformRequest:function(data, headersGetter){
+			console.log(data);
+			console.log(headersGetter);
 			var result = JSON.stringify(data);
 			console.log("Result:" + result);             
 		}}, 
@@ -14,3 +16,22 @@ mftApp.factory('UserAuth', ['$resource', function($resource) {
 		query: {method:'GET', isArray:false}});	
 	return getUserInfo;
 }]);
+
+
+/*services.factory('Auth', ['Base64', '$cookieStore', '$http', function (Base64, $cookieStore, $http) {
+    // initialize to whatever is in the cookie, if anything
+    $http.defaults.headers.common['Authorization'] = 'Basic ' + $cookieStore.get('authdata');
+ 
+    return {
+        setCredentials: function (username, password) {
+            var encoded = Base64.encode(username + ':' + password);
+            $http.defaults.headers.common.Authorization = 'Basic ' + encoded;
+            $cookieStore.put('authdata', encoded);
+        },
+        clearCredentials: function () {
+            document.execCommand("ClearAuthenticationCache");
+            $cookieStore.remove('authdata');
+            $http.defaults.headers.common.Authorization = 'Basic ';
+        }
+    };
+}]);*/
