2459c659b6e4b103e3027a5e1af2db5204193963
Aaron Lieberman <aaron@bigcompass.com>
2014-02-04 16:51:10 -0700

    Comment Fix

--- app/js/controllers/CommentIndexCtrl.js
+++ app/js/controllers/CommentIndexCtrl.js
@@ -11,6 +11,7 @@ mftApp.controller('CommentIndexCtrl', ['$scope', '$resource', 'Comment', '$route
 	$scope.dateFormat = new Date().getTime();      //'M/d/yy h:mm:ss a';
 	$scope.commentList = [];
 	Comment.get({id: $routeParams.id}, function(results){
+		console.log("1");
 		$scope.commentList = results.results;
 		$scope.commentList.sort(function(a,b){
 		  a = new Date(a.CREATED_AT);
@@ -23,25 +24,27 @@ mftApp.controller('CommentIndexCtrl', ['$scope', '$resource', 'Comment', '$route
 	$scope.postCommentSuccess = false;
 	$scope.postCommentFailure = false;
 	$scope.submitted = false;
-	
+	console.log("2");
 	$scope.save = function() {
+		console.log("3");
 		$scope.commentForm.$setPristine();
 		$scope.submitted = true;
 		if($scope.commentForm.$valid) {
-		
+		console.log("4");
 		Comment.post({id:$routeParams.id, CURRENT_USER:$scope.user.email, COMMENT_FEED:$scope.commentList.COMMENT_FEED}, 
 		$scope.commentList, 
 		function() {
+			console.log("5");
 			$scope.submitted = false;
-		    /*Comment.get({id: $routeParams.id}, function(results){
+		    Comment.get({id: $routeParams.id}, function(results){
+			console.log("6");
 				$scope.commentList = results.results;
 				$scope.commentList.sort(function(a,b){
 				  a = new Date(a.CREATED_AT);
 				  b = new Date(b.CREATED_AT);
 				  return a<b?1:a>b?-1:0;
 				});
-			});*/
-			route.reload();
+			});
 			$scope.commentList.COMMENT_FEED = "";
 			$scope.postCommentSuccess = true;
 		},
